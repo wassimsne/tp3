ca6a52bd8ecf283dd669b2ab6a669cf48033bec2
wassim <wassimsne@yahoo.fr>
2023-05-18 22:47:01 +0200

    Add project files.

---
+++ tp3/Program.cs
@@ -0,0 +1,73 @@
+﻿// See https://aka.ms/new-console-template for more information
+using tp3.StringExtension;
+
+Console.WriteLine("Hello, World!");
+"Debut".dump();
+//CalculSync();
+//Task.Run( () =>  Calculaasync());
+
+Task<int> x= Calculerasync();
+x.ContinueWith(t => Afficher(t.Result));
+CalculSync();
+
+
+
+//await Calculaasync();   
+"Fin".dump();
+Console.ReadLine();
+
+
+
+void CalculSync()
+{
+    "DebutCalculsync".dump(ConsoleColor.Blue);
+    for (int i=0; i < 10000; i++)
+    {
+        for (int j = 0; j < 100000; j++) ;
+        
+    }
+    "FinCalculSimple".dump(ConsoleColor.Green);
+}
+int CalculsSync()
+{
+    "DebutCalculasync".dump(ConsoleColor.Blue);
+    for (int i = 0; i < 10000; i++)
+    {
+        for (int j = 0; j < 100000; j++) ;
+
+    }
+    "FinCalculSimple".dump(ConsoleColor.Green);
+    return (10);
+}
+
+Task Calculaasync()//Sans ype de retour
+{
+    "DebutCalculasync".dump(ConsoleColor.Red);
+    for (int i = 0; i < 10000; i++)
+    {
+        for (int j = 0; j < 10000; j++) ;
+
+    }
+    "FinCalculSimple".dump(ConsoleColor.Yellow);
+
+    return Task.CompletedTask;
+}
+Task<int> Calculerasync()//Avec type de retour
+{
+
+    "DebutCalculasync".dump(ConsoleColor.Red);
+    for (int i = 0; i < 10000; i++)
+    {
+        for (int j = 0; j < 10000; j++) ;
+
+    }
+    "FinCalculSimple".dump(ConsoleColor.Yellow);
+
+    return Task.FromResult(10);
+}  
+void Afficher(int resulatat)
+{
+    "Le resulaat est ".dump(ConsoleColor.Red);
+    int x=150 * resulatat;
+    Console.WriteLine(x);
+}
---
+++ tp3/StringExtension/StringExt.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace tp3.StringExtension
+{
+    public static  class StringExt
+    {
+        public static void dump(this string str, ConsoleColor color = ConsoleColor.White)
+        {
+            if (str != null)
+            {
+                Console.ForegroundColor = color;
+                Console.WriteLine(str);
+            }
+
+        }
+    }
+}
